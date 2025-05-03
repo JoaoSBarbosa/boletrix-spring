9ec95ca7128af96d5efa165d1bb995cc79b4e7e3
João S. Barbosa <contato.jsbarbosa@gmail.com>
2025-05-02 21:51:39 -0300

    :tada: inicialização do projeto ee strutura

---
+++ src/main/java/com/joaobarbosade/boletrix/BoletrixApplication.java
@@ -0,0 +1,13 @@
+package com.joaobarbosade.boletrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoletrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoletrixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaobarbosade/boletrix/BoletrixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaobarbosade.boletrix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoletrixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
